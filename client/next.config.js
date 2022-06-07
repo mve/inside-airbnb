832a57c192765594f678c49b59ab71fee6e9b8f7
Mike van Egmond <mikevanegmond1998@outlook.com>
2022-06-07 11:05:37 +0200

    strict content security policy added to next js app.

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,53 @@
+const ContentSecurityPolicy = `
+  default-src 'self' *.auth0.com api.mapbox.com events.mapbox.com 'unsafe-eval' 'unsafe-inline' data: blob: https://inside-airbnb-mve-api.azurewebsites.net;
+`
+
+const securityHeaders = [
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  }
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com', 's.gravatar.com', 'a0.muscache.com']
-  }
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
